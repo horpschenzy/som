59c4a4cf464ee1b05a8a04508d54cb8edb4bdc4a
Babatope Oni <6229996+babatopeoni@users.noreply.github.com>
2021-06-17 20:34:22 +0100

    add datatable to payments

--- resources/views/admin/transaction.blade.php
+++ resources/views/admin/transaction.blade.php
@@ -11,8 +11,8 @@
                         <h4 class="card-title">Payment for School of Ministry</h4>
                        <p class="card-title-desc">All payments must be made before 29th of June.</p>
 
-                        <div class="table-responsive">
-                            <table class="table table-bordered border-primary mb-0">
+                        <div class="table">
+                            <table id="datatable-buttons" class="table table-bordered border-primary mb-0">
 
                                 <thead>
                                     <tr>
@@ -59,24 +59,39 @@
 </div>
 
 @endsection
+
+
 @push('scripts')
-
 <!-- JAVASCRIPT -->
-<script src="{{ asset('assets/libs/jquery/jquery.min.js')}}"></script>
-<script src="{{asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-<script src="{{asset('assets/libs/metismenu/metisMenu.min.js')}}"></script>
-<script src="{{asset('assets/libs/simplebar/simplebar.min.js')}}"></script>
-<script src="{{asset('assets/libs/node-waves/waves.min.js')}}"></script>
-<script src="{{asset('assets/libs/jquery-sparkline/jquery.sparkline.min.js')}}"></script>
+<script src="assets/libs/jquery/jquery.min.js"></script>
+<script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/libs/metismenu/metisMenu.min.js"></script>
+<script src="assets/libs/simplebar/simplebar.min.js"></script>
+<script src="assets/libs/node-waves/waves.min.js"></script>
+<script src="assets/libs/jquery-sparkline/jquery.sparkline.min.js"></script>
 
+<!-- Required datatable js -->
+<script src="assets/libs/datatables.net/js/jquery.dataTables.min.js"></script>
+<script src="assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
+<!-- Buttons examples -->
+<script src="assets/libs/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
+<script src="assets/libs/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js"></script>
+<script src="assets/libs/jszip/jszip.min.js"></script>
+<script src="assets/libs/pdfmake/build/pdfmake.min.js"></script>
+<script src="assets/libs/pdfmake/build/vfs_fonts.js"></script>
+<script src="assets/libs/datatables.net-buttons/js/buttons.html5.min.js"></script>
+<script src="assets/libs/datatables.net-buttons/js/buttons.print.min.js"></script>
+<script src="assets/libs/datatables.net-buttons/js/buttons.colVis.min.js"></script>
+<!-- Responsive examples -->
+<script src="assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
+<script src="assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js"></script>
 
-
-<!-- plugin js -->
-<script src="{{ asset('assets/libs/moment/min/moment.min.js')}}"></script>
-<script src="{{ asset('assets/libs/jquery-ui-dist/jquery-ui.min.js')}}"></script>
+<!-- Datatable init js -->
+<script src="assets/js/pages/datatables.init.js"></script>
 
 <!-- App js -->
-<script src="{{asset('assets/js/app.js')}}"></script>
+<script src="assets/js/app.js"></script>
+
 
 
 @endpush
