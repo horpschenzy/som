0dffbd24c44e36a22b2eb9007c6a4bbbf9dbf8fe
Sisanmi Smith <sisanmi.smith@makarioworks.com>
2021-06-17 23:09:37 +0100

    paidstudent updated to datatable

--- resources/views/admin/paidstudent.blade.php
+++ resources/views/admin/paidstudent.blade.php
@@ -1,4 +1,13 @@
 @extends('admin.layouts.app')
+@push('extra-css')
+<!-- DataTables -->
+<link href="assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+<link href="assets/libs/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+
+<!-- Responsive datatable examples -->
+<link href="assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+
+@endpush
 @section('content')
  <!-- account setting page start -->
 
@@ -29,14 +38,13 @@
                     <div class="col-12">
                         <div class="card">
                             <div class="card-body">
-                                <table id="datatable" class="table table-bordered dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
+                                <table id="datatable-buttons" class="table table-striped table-bordered dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
                                     <thead>
                                         <tr>
                                             <th>Surname</th>
                                             <th>Firstname</th>
                                             <th>Amount</th>
                                             <th>Description</th>
-                                            <th>Center</th>
                                         </tr>
                                     </thead>
 
@@ -86,10 +94,24 @@
 <script src="{{asset('assets/libs/node-waves/waves.min.js')}}"></script>
 <script src="{{asset('assets/libs/jquery-sparkline/jquery.sparkline.min.js')}}"></script>
 
+<!-- Required datatable js -->
+<script src="assets/libs/datatables.net/js/jquery.dataTables.min.js"></script>
+<script src="assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
+<!-- Buttons examples -->
+<script src="assets/libs/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
+<script src="assets/libs/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js"></script>
+<script src="assets/libs/jszip/jszip.min.js"></script>
+<script src="assets/libs/pdfmake/build/pdfmake.min.js"></script>
+<script src="assets/libs/pdfmake/build/vfs_fonts.js"></script>
+<script src="assets/libs/datatables.net-buttons/js/buttons.html5.min.js"></script>
+<script src="assets/libs/datatables.net-buttons/js/buttons.print.min.js"></script>
+<script src="assets/libs/datatables.net-buttons/js/buttons.colVis.min.js"></script>
+<!-- Responsive examples -->
+<script src="assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
+<script src="assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js"></script>
 
-
-
-
+<!-- Datatable init js -->
+<script src="assets/js/pages/datatables.init.js"></script>
 <!-- App js -->
 <script src="{{asset('assets/js/app.js')}}"></script>
 
