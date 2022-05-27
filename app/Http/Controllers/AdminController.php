9b144b971faee133852446b34e4f065df7fc8eef
Babatope Oni <babatopeoni@gmail.com>
2022-05-27 09:36:36 +0100

    Fix bug

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -13,6 +13,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Http;
 
 class AdminController extends Controller
 {
