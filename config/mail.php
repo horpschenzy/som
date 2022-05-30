ed86a34bc7c92d9c01934b4865bd438769ed2607
Babatope Oni <babatopeoni@gmail.com>
2022-05-30 04:43:00 +0100

    Add mail driver for mailjet

--- config/mail.php
+++ config/mail.php
@@ -70,6 +70,38 @@ return [
         'array' => [
             'transport' => 'array',
         ],
+
+        'mailjet' => [
+            'key' => env('MAILJET_APIKEY'),
+            'secret' => env('MAILJET_APISECRET'),
+            'transactional' => [
+                'call' => true,
+                'options' => [
+                    'url' => 'api.mailjet.com',
+                    'version' => 'v3.1',
+                    'call' => true,
+                    'secured' => true
+                ]
+            ],
+            'common' => [
+                'call' => true,
+                'options' => [
+                    'url' => 'api.mailjet.com',
+                    'version' => 'v3',
+                    'call' => true,
+                    'secured' => true
+                ]
+            ],
+            'v4' => [
+                'call' => true,
+                'options' => [
+                    'url' => 'api.mailjet.com',
+                    'version' => 'v4',
+                    'call' => true,
+                    'secured' => true
+                ]
+            ],
+        ]
     ],
 
     /*
